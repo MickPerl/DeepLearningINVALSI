838f55eef4c15dbc8808061a43a6abffbf181e67
TommasoAzz <azzalintommaso@gmail.com>
2021-09-18 15:29:38 +0200

    Rimossi vecchi sbatch, vecchio base.py, vecchia configurazione

--- src/config.py
+++ src/config.py
@@ -1,20 +1,11 @@
 from tensorflow.keras import optimizers
 from os import getenv
 
-# both
-LEARNING_RATE = float(getenv(key="LEARNING_RATE", default="0.001"))  # --learningrate floatnum
-DROPOUT_LAYER = eval(getenv(key="DROPOUT_LAYER", default="False"))  # --dropout
-EPOCH = int(getenv(key="EPOCH", default="50"))  # --epoch intnum
-NEURONS = int(getenv(key="NEURONS", default="128"))  # --neurons intnum
-BATCH_SIZE = int(getenv(key="BATCH_SIZE", default="32"))  # --batchsize intnum
-# base.py
-OUTPUT_ACTIVATION_FUNCTION = getenv(key="OUTPUT_ACTIVATION_FUNCTION", default="softmax")  # --activation nomefunzione
-DENSE_LAYER_ACTIVATION = getenv(key="DENSE_LAYER_ACTIVATION", default="relu")  # hidden layers' activation function
-AP_DATASET_PATH = getenv(key="AP_DATASET_PATH", default="/Users/marco/Documents/Università/Intelligenza artificiale/project.nosync/MachineLearningProject/dataset_with_AP.csv")  # --dataset nomefile
-SMALL_DATASET = eval(getenv(key="SMALL_DATASET", default="False"))  # --small
-MODEL = int(getenv(key="MODEL", default="1"))  # --model intnum
-OPTIMIZER = optimizers.Adam(learning_rate=LEARNING_RATE)  # --optimizer nomeottimizzatore
-# invalsi.py
+LEARNING_RATE = float(getenv(key="LEARNING_RATE", default="0.001"))
+DROPOUT_LAYER = eval(getenv(key="DROPOUT_LAYER", default="False"))
+EPOCH = int(getenv(key="EPOCH", default="50"))
+NEURONS = int(getenv(key="NEURONS", default="128"))
+BATCH_SIZE = int(getenv(key="BATCH_SIZE", default="32"))
 ORIGINAL_DATASET = getenv(key="ORIGINAL_DATASET", default="../nuovi_dataset/original_dataset.csv")
 CLEANED_DATASET = getenv(key="CLEANED_DATASET", default="../nuovi_dataset/cleaned_dataset.csv")
 CLEANED_DATASET_WITH_AP = getenv(key="CLEANED_DATASET_WITH_AP", default="../nuovi_dataset/dataset_ap.csv")
